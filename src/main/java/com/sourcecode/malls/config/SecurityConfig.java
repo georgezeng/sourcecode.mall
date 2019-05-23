7639365eb9352ffd438c24548304a3f1efc3ae97
georgezeng <zzeng@bglcorp.com.au>
2019-05-23 11:51:51 +0800

    no message

--- src/main/java/com/sourcecode/malls/config/SecurityConfig.java
+++ src/main/java/com/sourcecode/malls/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig extends BaseSecurityConfig {
 		wechatAuthenticationFilter.setSessionAuthenticationStrategy(sessionStrategy);
 		http.addFilterBefore(sessionFilter, FilterSecurityInterceptor.class);
 		http.addFilterBefore(verifyCodeAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-		http.addFilterAfter(wechatAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		http.addFilterBefore(wechatAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		http.addFilterAt(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 
