2519396a069ecd33587b34726833ef2543f5fa1f
georgezeng <zzeng@bglcorp.com.au>
2019-05-24 18:22:45 +0800

    no message

--- src/main/java/com/sourcecode/malls/config/SecurityConfig.java
+++ src/main/java/com/sourcecode/malls/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.session.ChangeSessionIdAuthenticationStrategy;
@@ -16,6 +17,7 @@ import com.sourcecode.malls.web.security.filter.ClientSessionFilter;
 import com.sourcecode.malls.web.security.filter.ClientUsernamePasswordAuthenticationFilter;
 import com.sourcecode.malls.web.security.filter.ClientVerifyCodeAuthenticationFilter;
 import com.sourcecode.malls.web.security.filter.ClientWechatAuthenticationFilter;
+import com.sourcecode.malls.web.security.filter.TestFilter;
 import com.sourcecode.malls.web.security.rememberme.ClientRememberMeServices;
 
 @Configuration
@@ -38,6 +40,9 @@ public class SecurityConfig extends BaseSecurityConfig {
 
 	@Autowired
 	private ClientRememberMeServices rememberMeServices;
+	
+	@Autowired
+	private TestFilter testFilter;
 
 	private CompositeSessionAuthenticationStrategy sessionStrategy = new CompositeSessionAuthenticationStrategy(
 			Arrays.asList(new ChangeSessionIdAuthenticationStrategy()));
@@ -67,6 +72,7 @@ public class SecurityConfig extends BaseSecurityConfig {
 		wechatAuthenticationFilter.setAuthenticationSuccessHandler(successHandler);
 		wechatAuthenticationFilter.setAuthenticationFailureHandler(failureHandler);
 		wechatAuthenticationFilter.setSessionAuthenticationStrategy(sessionStrategy);
+		http.addFilterBefore(testFilter, ChannelProcessingFilter.class);
 		http.addFilterBefore(sessionFilter, FilterSecurityInterceptor.class);
 		http.addFilterBefore(verifyCodeAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		http.addFilterBefore(wechatAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
