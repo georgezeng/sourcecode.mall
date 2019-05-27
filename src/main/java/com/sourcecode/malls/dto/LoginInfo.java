e96d8ce356e0eedadbf835ba422e121ad643cce8
georgezeng <zzeng@bglcorp.com.au>
2019-05-27 15:55:33 +0800

    no message

--- src/main/java/com/sourcecode/malls/dto/LoginInfo.java
+++ src/main/java/com/sourcecode/malls/dto/LoginInfo.java
@@ -3,6 +3,15 @@ package com.sourcecode.malls.dto;
 public class LoginInfo {
 	private String username;
 	private String password;
+	private String toekn;
+
+	public String getToekn() {
+		return toekn;
+	}
+
+	public void setToekn(String toekn) {
+		this.toekn = toekn;
+	}
 
 	public String getUsername() {
 		return username;
