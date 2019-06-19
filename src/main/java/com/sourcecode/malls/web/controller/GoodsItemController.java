e209db5e6db0baab0cb4e57601200d66ec2064ac
georgezeng <zzeng@bglcorp.com.au>
2019-06-19 17:29:16 +0800

    no message

--- src/main/java/com/sourcecode/malls/web/controller/GoodsItemController.java
+++ src/main/java/com/sourcecode/malls/web/controller/GoodsItemController.java
@@ -26,6 +26,7 @@ import com.sourcecode.malls.dto.query.PageInfo;
 import com.sourcecode.malls.repository.jpa.impl.goods.GoodsItemEvaluationRepository;
 import com.sourcecode.malls.repository.jpa.impl.goods.GoodsSpecificationDefinitionRepository;
 import com.sourcecode.malls.service.impl.GoodsItemService;
+import com.sourcecode.malls.web.security.filter.UserSessionFilter;
 
 @RestController
 @RequestMapping(path = "/goods/item")
@@ -45,8 +46,10 @@ public class GoodsItemController {
 	public ResultBean<GoodsItemDTO> list(@PathVariable("id") Long categoryId, @PathVariable("type") String type,
 			@RequestBody PageInfo pageInfo) {
 		Page<GoodsItem> result = service.findByCategory(ClientContext.getMerchantId(), categoryId, type, pageInfo);
-		return new ResultBean<>(
+		ResultBean<GoodsItemDTO> data = new ResultBean<>(
 				result.getContent().stream().map(it -> it.asDTO(false, false)).collect(Collectors.toList()));
+		UserSessionFilter.V.set(System.currentTimeMillis());
+		return data;
 	}
 
 	@RequestMapping(path = "/definitions/load")
