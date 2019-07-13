bf6c837bef6092cbf065fad046193a26bb6c82a1
georgezeng <zzeng@bglcorp.com.au>
2019-07-13 22:13:25 +0800

    no message

--- src/main/java/com/github/wxpay/sdk/WePayConfig.java
+++ src/main/java/com/github/wxpay/sdk/WePayConfig.java
@@ -3,19 +3,25 @@ package com.github.wxpay.sdk;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sourcecode.malls.dto.setting.DeveloperSettingDTO;
 
 public class WePayConfig extends WXPayConfig {
+
 	private String appId;
 	private String key;
 	private String mchId;
 	private ByteArrayInputStream bis;
+	private String domain;
 
-	public WePayConfig(DeveloperSettingDTO info) {
+	public WePayConfig(DeveloperSettingDTO info, byte[] cert, String domain) {
 		this.appId = info.getAccount();
 		this.key = info.getSecret();
 		this.mchId = info.getMch();
-		this.bis = new ByteArrayInputStream(info.getCert());
+		this.bis = new ByteArrayInputStream(cert);
+		this.domain = domain;
 	}
 
 	@Override
@@ -40,7 +46,20 @@ public class WePayConfig extends WXPayConfig {
 
 	@Override
 	IWXPayDomain getWXPayDomain() {
-		return null;
+		return new IWXPayDomain() {
+			private Logger logger = LoggerFactory.getLogger(getClass());
+
+			@Override
+			public void report(String domain, long elapsedTimeMillis, Exception ex) {
+				logger.error("[WePay Error] - " + domain + ": " + ex.getMessage(), ex);
+			}
+
+			@Override
+			public DomainInfo getDomain(WXPayConfig config) {
+				return new DomainInfo(domain, true);
+			}
+
+		};
 	}
 
 }
--- src/main/java/com/sourcecode/malls/service/impl/WechatSettingService.java
+++ src/main/java/com/sourcecode/malls/service/impl/WechatSettingService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.github.wxpay.sdk.WePayConfig;
+import com.sourcecode.malls.domain.merchant.MerchantShopApplication;
 import com.sourcecode.malls.dto.setting.DeveloperSettingDTO;
+import com.sourcecode.malls.repository.jpa.impl.merchant.MerchantShopApplicationRepository;
 import com.sourcecode.malls.util.AssertUtil;
 
 @Component
@@ -14,11 +16,14 @@ public class WechatSettingService {
 	@Autowired
 	private MerchantSettingService service;
 
+	@Autowired
+	private MerchantShopApplicationRepository shopRepository;
+
 	public WePayConfig createWePayConfig(Long merchantId) throws Exception {
 		Optional<DeveloperSettingDTO> info = service.loadWechatGzh(merchantId);
-		AssertUtil.assertTrue(info.isPresent(), "未找到商家的微信信息");
-		DeveloperSettingDTO dto = info.get();
-		dto.setCert(service.loadWepayCert(merchantId));
-		return new WePayConfig(dto);
+		AssertUtil.assertTrue(info.isPresent(), "未找到商户的微信信息");
+		Optional<MerchantShopApplication> shop = shopRepository.findByMerchantId(merchantId);
+		AssertUtil.assertTrue(shop.isPresent(), "未找到商户的信息");
+		return new WePayConfig(info.get(), service.loadWepayCert(merchantId), shop.get().getDomain());
 	}
 }
