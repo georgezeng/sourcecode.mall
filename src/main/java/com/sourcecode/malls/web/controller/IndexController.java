5183ac0c21cc8fc25a51bc45d62cb3b22482a3ad
georgezeng <zzeng@bglcorp.com.au>
2019-07-18 21:49:38 +0800

    no message

--- src/main/java/com/sourcecode/malls/web/controller/IndexController.java
+++ src/main/java/com/sourcecode/malls/web/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.sourcecode.malls.web.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,13 @@ public class IndexController {
 
 	@RequestMapping(path = "/index")
 	public String index(HttpServletRequest request) {
-		return String.format("<script>window.location.href='%s'</script>", String.format("%s/#/Home", request.getParameter("origin")));
+		return String.format("<script>window.location.href='%s'</script>",
+				String.format("%s/#/Home", request.getParameter("origin")));
+	}
+
+	@RequestMapping(path = "/index/jump")
+	public String index(HttpServletRequest request, @RequestParam("url") String url) {
+		return String.format("<script>window.location.href='%s'</script>", url);
 	}
 
 }
