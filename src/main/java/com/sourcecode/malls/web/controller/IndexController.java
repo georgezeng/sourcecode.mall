00c38ad9c02d69b9e7cc7991fcdaa41ff8eaba1a
georgezeng <zzeng@bglcorp.com.au>
2019-07-26 21:42:18 +0800

    no message

--- src/main/java/com/sourcecode/malls/web/controller/IndexController.java
+++ src/main/java/com/sourcecode/malls/web/controller/IndexController.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sourcecode.malls.context.ClientContext;
+
 @RestController
 public class IndexController {
 
 	@RequestMapping(path = "/index")
 	public String index(HttpServletRequest request, @RequestParam("url") String url) {
+		if (ClientContext.get() != null) {
+			url = url.replace("#", "?uid=" + ClientContext.get().getId() + "#");
+		}
 		return String.format("<script>window.location.href='%s'</script>", url);
 	}
 
